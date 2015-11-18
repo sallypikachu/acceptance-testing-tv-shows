9b00295bd12c7b66f3b878e7c56a3a188fbfc334
sallypikachu <sallypikachu@gmail.com>
2015-11-18 12:40:59 -0500

    Initial commit

---
+++ server.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'csv'
+require_relative "app/models/television_show"
+
+set :views, File.join(File.dirname(__FILE__), "app/views")
+
+get '/television_shows' do
+  @shows = []
+
+  CSV.foreach('television_shows.csv', headers: true, header_converters: :symbol) do |row|
+    show = row.to_hash
+    @shows << show
+  end
+  erb :index
+end
+
+get '/television_shows/new' do
+  erb :new
+end
+
+post '/television_shows/new' do
+  title = params['title']
+  network = params['network']
+  starting_year = params['starting_year']
+  synopsis = params['synopsis']
+  genre = params['genre']
+
+  data = [title, network, starting_year, synopsis, genre]
+
+  if data.any? { |item| item == nil || item == ""}
+    erb :error
+  else
+    CSV.open('television_shows.csv', 'a') do |file|
+    file << data
+  end
+    redirect '/television_shows'
+  end
+end
